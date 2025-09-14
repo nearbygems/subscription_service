b2153b25680164d65792b2fa8bd4e12779c0d65c
bergen <Bergen.Asym@kaspi.kz>
2025-09-14 19:50:19 +0500

    added config.go

---
+++ internal/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import "os"
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+}
+
+func Load() *Config {
+	return &Config{
+		DBHost:     getEnv("DB_HOST", "db"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", "password"),
+		DBName:     getEnv("DB_NAME", "subscriptions_db"),
+	}
+}
+
+func getEnv(key, def string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return def
+}
